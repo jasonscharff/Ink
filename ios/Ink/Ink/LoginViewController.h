f15f28a6f83b18b0c9fcdcd83e95672f15d5696c
jasonscharff <jasonscharff@gmail.com>
2016-01-22 23:20:03 -0500

    Create a project.

---
+++ ios/Ink/Ink/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Ink
+//
+//  Created by Jason Scharff on 1/22/16.
+//  Copyright © 2016 Jason Scharff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ ios/Ink/Ink/LoginViewController.m
@@ -0,0 +1,23 @@
+//
+//  LoginViewController.m
+//  Ink
+//
+//  Created by Jason Scharff on 1/22/16.
+//  Copyright © 2016 Jason Scharff. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  self.view.backgroundColor = [UIColor whiteColor];
+}
+
+
+@end
